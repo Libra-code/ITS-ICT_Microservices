06385fe4fd05fb157bf0ed8b44d38963331a0436
Libra-code <78862538+Libra-code@users.noreply.github.com>
2021-12-10 17:02:16 +0100

    Update start-infra.sh
    Comment to work on VM LINUX

--- labs/02-eCommerce_microservices/start-infra.sh
+++ labs/02-eCommerce_microservices/start-infra.sh
@@ -1,4 +1,7 @@
 docker-compose up -d zookeeper
 docker-compose up -d kafka
-docker-compose up -d postgres_db
+docker-compose up -d postgres_db 
+#lsof -i tcp:5432
+#sudo lsof -i tcp:5432
+#sudo kill -9 18933
 docker-compose up -d mongodb_db
